c70be13d4664f6bc23b40647fdb37c09c0bac856
John Asare <137245223+asarejohn001@users.noreply.github.com>
2024-07-20 20:59:38 -0400

    Import Microsoft gragh module and connect to m365 tenant

--- UserAccountProvision.ps1
+++ UserAccountProvision.ps1
@@ -3,6 +3,25 @@ Name: John Asare
 Date: 2024-07-20
 
 User account provision. Read more about this script at https://github.com/asarejohn001/UserAccountProvisioning
-
 #>
 
+# Install Microsoft Graph PowerShell module if you haven't already
+Install-Module Microsoft.Graph -Scope CurrentUser
+
+# Import the Microsoft Graph PowerShell module
+Import-Module Microsoft.Graph
+
+# Define the scopes (permissions) needed for the connection
+$scopes = @(
+    "User.Read.All",
+    "Group.Read.All",
+    "Directory.Read.All",
+    "Directory.AccessAsUser.All"
+)
+
+# Connect to Microsoft Graph
+Connect-MgGraph -Scopes $scopes
+
+# Verify the connection
+$tenantDetails = Get-MgOrganization
+$tenantDetails.DisplayName
