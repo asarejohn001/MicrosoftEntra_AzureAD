c5c40ef865b66c3b6f974f4a3ce960638b573ec1
John Asare <137245223+asarejohn001@users.noreply.github.com>
2024-07-20 22:00:25 -0400

    migrate from microsoft azure ad to microsoft grapgh

--- UserAccountProvision.ps1
+++ UserAccountProvision.ps1
@@ -5,19 +5,20 @@ Date: 2024-07-20
 User account provision. Read more about this script at https://github.com/asarejohn001/UserAccountProvisioning
 #>
 
-# Install the AzureAD module if you haven't already
-Install-Module AzureAD
-Install-Module AzureAD.Standard.Preview
+# Import the Microsoft Graph PowerShell module
+Import-Module Microsoft.Graph
 
-# Import the Azure AD module
-Import-Module AzureAD
-Import-Module AzureAD.Standard.Preview
+# Define the scopes (permissions) needed for the connection
+$scopes = @(
+    "User.ReadWrite.All",
+    "Directory.ReadWrite.All"
+)
 
-# Connect to Azure AD
-Connect-AzureAD
+# Connect to Microsoft Graph
+Connect-MgGraph -Scopes $scopes
 
 # Path to the CSV file
-$csvPath = "./New Hires.csv"
+$csvPath = "/path/to/your/users.csv"
 
 # Import the CSV file
 $users = Import-Csv -Path $csvPath
@@ -25,28 +26,32 @@ $users = Import-Csv -Path $csvPath
 # Loop through each user in the CSV
 foreach ($user in $users) {
     # Create a password profile for the user
-    $passwordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
-    $passwordProfile.Password = "InitialPassword123!" # You can generate a more secure password or handle this as needed
-    $passwordProfile.ForceChangePasswordNextLogin = $true
+    $passwordProfile = @{
+        forceChangePasswordNextSignIn = $true
+        password = "InitialPassword123!" # You can generate a more secure password or handle this as needed
+    }
 
     # Generate user principal name (UPN) and mail nickname
     $upn = "$($user.FirstName).$($user.LastName)@yourdomain.com"
     $mailNickname = "$($user.FirstName)$($user.LastName)"
 
     # Create the user
-    New-AzureADUser -DisplayName "$($user.FirstName) $($user.LastName)" `
-                    -PasswordProfile $passwordProfile `
-                    -UserPrincipalName $upn `
-                    -MailNickname $mailNickname `
-                    -AccountEnabled $false `
-                    -JobTitle $user.JobTitle `
-                    -CompanyName $user.CompanyName `
-                    -Department $user.Department `
-                    -EmployeeId $user.EmployeeID `
-                    -EmployeeType $user.EmployeeType `
-                    -HireDate $user.EmployeeHireDate `
-                    -OfficeLocation $user.OfficeLocation `
-                    -UsageLocation "US"
+    $userDetails = @{
+        accountEnabled = $true
+        displayName = "$($user.FirstName) $($user.LastName)"
+        mailNickname = $mailNickname
+        userPrincipalName = $upn
+        jobTitle = $user.JobTitle
+        companyName = $user.CompanyName
+        department = $user.Department
+        employeeId = $user.EmployeeID
+        employeeType = $user.EmployeeType
+        hireDate = $user.EmployeeHireDate
+        officeLocation = $user.OfficeLocation
+        passwordProfile = $passwordProfile
+    }
+
+    New-MgUser -BodyParameter $userDetails
 }
 
 # Output success message
